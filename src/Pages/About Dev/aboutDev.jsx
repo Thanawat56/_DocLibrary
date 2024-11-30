f8c27ac3ec4ab139bdc9e9e2353a8d3c703c336a
Thanawat56 <Thanawat.nal@spumail.net>
2024-11-30 16:35:03 +0700

    delete

--- src/Pages/About Dev/aboutDev.jsx
+++
@@ -1,8 +0,0 @@
-function AboutDev() {
-    return ( <div>
-        <h1>About Dev Project Doc-Library Group1</h1>
-        <img src="./3.jpg" alt="" width={1500}/>
-    </div> );
-}
-
-export default AboutDev;
